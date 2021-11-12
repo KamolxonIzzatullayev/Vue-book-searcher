9910e2c4809d5f4a41f50b1bf9957ab10a226fc8
unknown <KamolxonIzzatullayev>
2021-11-12 15:55:44 +0500

    bugs fixed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,6 @@
     <div>
         <footer class="page-footer bottom font-small blue">
             <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© Powered By
-    <a href="https://developers.google.com/books/docs/overview"> Google Books API</a>
-  </div>
   <!-- Copyright -->
         </footer>
     </div>
